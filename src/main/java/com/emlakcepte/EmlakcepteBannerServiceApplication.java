395692413a11e81813ab214d40d556c1e5df1d62
cemdrman <cemdrman@gmail.com>
2022-12-26 18:36:52 +0300

    euraka client

--- src/main/java/com/emlakcepte/EmlakcepteBannerServiceApplication.java
+++ src/main/java/com/emlakcepte/EmlakcepteBannerServiceApplication.java
@@ -2,8 +2,10 @@ package com.emlakcepte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmlakcepteBannerServiceApplication {
 
 	public static void main(String[] args) {
